f5647b8df79e81c877d88cc9a5cda6f0a2157ee0
Hugo Hennetin <hugo.hennetin@proton.me>
2023-11-22 15:34:17 +0100

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,17 +1,29 @@
 #include <Wire.h>
 #include <Servo.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x20,16,2);
+int bp1 = 2;
+int button_state;
 int brochepot = A0;
 int valpot;
 int conversion;
 Servo servo;
+int convesion_old;
 
 void setup() {
-  servo.attach(2);
+  servo.attach(3);
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();
+  lcd.setCursor(1,0);
+  lcd.print("Hugo Hennetin");
 }
 
 void loop() {
   valpot = analogRead(brochepot);
   conversion = map(valpot, 0, 1024, 0, 180);
+  lcd.setCursor(1,1);
+  lcd.print("Potent.: " + String(conversion));
   servo.write(conversion);
 }
